ea173e02493272f2e92270c0154e0984778aef54
Angela Zhang <angelazhangyr@gmail.com>
2021-03-10 23:16:06 +0000

    format json in main

--- code/main.cpp
+++ code/main.cpp
@@ -1,6 +1,7 @@
 //HTTP credits: http://lcs.ios.ac.cn/~maxt/SPelton/reports/report-9aa0d3.html
 //JSON credits: https://github.com/nlohmann/json
 #include <iostream>
+#include <fstream>
 #include "./json.hpp"
 #include "./http_stuff.h"
 using namespace std;
@@ -9,10 +10,56 @@ using namespace nlohmann;
 int counter = 0;
 string moves[4] = {"up", "down", "left", "right"};
 
-int move();
+int init_data(const json data);
 int cycle = 0;
 
 int main(void) {
+  /* For testing only */
+  const json data = {
+    {"board",
+        {"food",
+        {{"x",0,"y",0},
+          {"x",5,"y",5},
+          {"x",9,"y",1},
+          {"x",10,"y",1}}},
+        {"hazards",{}},
+        {"height",11},
+        {"snakes",
+          {"body",
+            {{"x",1,},
+            {"y",10},
+            {"x",1,"y",9},
+            {"x",1,"y",8}}},
+          {"head",{"x",1,"y",10}},
+          {"health",91},
+          {"id","gs_pMjyVgp4FHkVmShkmpWV6SHW"},
+          {"latency","64"},
+          {"length",3},
+          {"name","nicole"},
+          {"shout",""},
+          {"width",11}}},
+    {"game",
+      {"id","dee22316-c8fc-46ab-a772-41eb6e85f741","ruleset"},
+      {"name","solo","version","v1.0.17"},
+      {"timeout",500}},
+    {"turn",9},
+    {"you",
+      {"body",
+        {{"x",1,"y",10},
+        {"x",1,"y",9},
+        {"x",1,"y",8}}},
+      {"head",{"x",1,"y",10}},
+      {"health",91},
+      {"id","gs_pMjyVgp4FHkVmShkmpWV6SHW"},
+      {"latency","64"},
+      {"length",3},
+      {"name","nicole"},
+      {"shout",""}}};
+  
+  int index = init_data(data);
+  return 0;
+} 
+/*
   httplib::Server svr;
   svr.Get("/", [](const auto &, auto &res) {
     string head = "default"; //TODO: Change head
@@ -28,9 +75,13 @@ int main(void) {
     res.set_content("ok", "text/plain");
   });
   svr.Post("/move", [](auto &req, auto &res){
+    // const json data;
+    // std::ifstream data_file("test.json", std::ifstream::binary);
+    // data_file >> data;
+
     const json data = json::parse(req.body);
-    cout << data;
-    cout << "\n\n";
+    // cout << data;
+    // cout << "\n\n";
     //You can get the "you" property like this:
     //data["you"];
     //Almost alike python dictionary parsing, but with a semicolon at the end of each line.
@@ -38,9 +89,10 @@ int main(void) {
     //Example:
     //you_struct you = data["you"];
     
-    int index = move(data);
+    // int index = move(data);
     res.set_content("{\"move\": \"" + moves[index] + "\"}", "text/plain");
   });
   svr.listen("0.0.0.0", 8080);
   cout << "Server started";
 }
+*/
--- code/move.cpp
+++ code/move.cpp
@@ -2,6 +2,7 @@
 //JSON credits: https://github.com/nlohmann/json
 #include <iostream>
 #include "./json.hpp"
+#include <vector>
 #include "./http_stuff.h"
 using namespace std;
 using namespace nlohmann;
@@ -9,8 +10,22 @@ using namespace nlohmann;
 extern string moves;
 extern int cycle;
 
-int move(const json data){
+int init_data(const json data){
+  cout << "\n\n" << data["board"]["food"]["x"] << "\n\n";
+  vector<int>food;
+  
+  // for (const auto& item : data["board"]["food"].items())
+  //   {
+  //      food.push_back(item);
+  //   }
+  // for (auto &f : food){
+  //   cout << f << endl;
+  // }
+  return 0;
+}
+
+int move(){
+  // cout << "data: " << data[1] << "\n";
   int index = cycle % 4;
   return index;
-  cout << "hello\n";
 }
