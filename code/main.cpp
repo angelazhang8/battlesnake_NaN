de3cabd452f0c5e479aa1f04facd75550d79070f
Angela Zhang <angelazhangyr@gmail.com>
2021-03-14 17:38:50 -0400

    next think of ways to not die

--- code/main.cpp
+++ code/main.cpp
@@ -1,64 +1,28 @@
-//HTTP credits: http://lcs.ios.ac.cn/~maxt/SPelton/reports/report-9aa0d3.html
-//JSON credits: https://github.com/nlohmann/json
-#include <iostream>
+// HTTP credits: http://lcs.ios.ac.cn/~maxt/SPelton/reports/report-9aa0d3.html
+// JSON credits: https://github.com/nlohmann/json
 #include <fstream>
-#include "./json.hpp"
+#include <iostream>
+#include <string>
+
 #include "./http_stuff.h"
+#include "./json.hpp"
 using namespace std;
 using namespace nlohmann;
 
 int counter = 0;
-string moves[4] = {"up", "down", "left", "right"};
+std::string moves[4] = {"up", "down", "left", "right"};
 
 int init_data(const json data);
 int cycle = 0;
 
 int main(void) {
   /* For testing only */
-  const json data = {
-    {"board",
-        {"food",
-        {{"x",0,"y",0},
-          {"x",5,"y",5},
-          {"x",9,"y",1},
-          {"x",10,"y",1}}},
-        {"hazards",{}},
-        {"height",11},
-        {"snakes",
-          {"body",
-            {{"x",1,},
-            {"y",10},
-            {"x",1,"y",9},
-            {"x",1,"y",8}}},
-          {"head",{"x",1,"y",10}},
-          {"health",91},
-          {"id","gs_pMjyVgp4FHkVmShkmpWV6SHW"},
-          {"latency","64"},
-          {"length",3},
-          {"name","nicole"},
-          {"shout",""},
-          {"width",11}}},
-    {"game",
-      {"id","dee22316-c8fc-46ab-a772-41eb6e85f741","ruleset"},
-      {"name","solo","version","v1.0.17"},
-      {"timeout",500}},
-    {"turn",9},
-    {"you",
-      {"body",
-        {{"x",1,"y",10},
-        {"x",1,"y",9},
-        {"x",1,"y",8}}},
-      {"head",{"x",1,"y",10}},
-      {"health",91},
-      {"id","gs_pMjyVgp4FHkVmShkmpWV6SHW"},
-      {"latency","64"},
-      {"length",3},
-      {"name","nicole"},
-      {"shout",""}}};
-  
+  auto data = R"(
+    {"board":{"food":[{"x":8,"y":2},{"x":10,"y":10},{"x":10,"y":4},{"x":6,"y":8},{"x":0,"y":0},{"x":2,"y":8},{"x":0,"y":6},{"x":4,"y":2}],"hazards":[],"height":11,"snakes":[{"body":[{"x":9,"y":10},{"x":9,"y":9},{"x":9,"y":8}],"head":{"x":9,"y":10},"health":91,"id":"gs_kTKcxB9w99BfGqD3rSbjtPhM","latency":"58","length":3,"name":"testsnake","shout":""},{"body":[{"x":1,"y":10},{"x":1,"y":9},{"x":1,"y":8}],"head":{"x":1,"y":10},"health":91,"id":"gs_XrdgvBhQGD34j86TRgFwP3D3","latency":"61","length":3,"name":"testsnake","shout":""},{"body":[{"x":5,"y":10},{"x":5,"y":9},{"x":5,"y":8},{"x":5,"y":7}],"head":{"x":5,"y":10},"health":95,"id":"gs_p4RGYfGGkCfjqBhYqVBRjQKM","latency":"120","length":4,"name":"testsnake","shout":""}],"width":11},"game":{"id":"e437218a-ddd4-486c-a18f-cb0af1ae05bc","ruleset":{"name":"standard","version":"v1.0.17"},"timeout":500},"turn":9,"you":{"body":[{"x":9,"y":10},{"x":9,"y":9},{"x":9,"y":8}],"head":{"x":9,"y":10},"health":91,"id":"gs_kTKcxB9w99BfGqD3rSbjtPhM","latency":"58","length":3,"name":"testsnake","shout":""}}
+  )"_json;
   int index = init_data(data);
   return 0;
-} 
+}
 /*
   httplib::Server svr;
   svr.Get("/", [](const auto &, auto &res) {
@@ -66,7 +30,9 @@ int main(void) {
     string tail = "default"; //TODO: Change tail
     string author = "NaN";
     string color = "#888888";  //TODO: Change a hex color
-    res.set_content("{\"apiversion\":\"1\", \"head\":\"" + head + "\", \"tail\":\"" + tail + "\", \"color\":\"" + color + "\", " + "\"author\":\"" + author + "\"}", "text/json");
+    res.set_content("{\"apiversion\":\"1\", \"head\":\"" + head + "\",
+\"tail\":\"" + tail + "\", \"color\":\"" + color + "\", " + "\"author\":\"" +
+author + "\"}", "text/json");
   });
   svr.Post("/end", [](const auto &, auto &res){
     res.set_content("ok", "text/plain");
@@ -84,11 +50,12 @@ int main(void) {
     // cout << "\n\n";
     //You can get the "you" property like this:
     //data["you"];
-    //Almost alike python dictionary parsing, but with a semicolon at the end of each line.
+    //Almost alike python dictionary parsing, but with a semicolon at the end of
+each line.
     //You might need to make some structs to store some data in a variable
     //Example:
     //you_struct you = data["you"];
-    
+
     // int index = move(data);
     res.set_content("{\"move\": \"" + moves[index] + "\"}", "text/plain");
   });
--- code/move.cpp
+++ code/move.cpp
@@ -1,31 +1,88 @@
-//HTTP credits: http://lcs.ios.ac.cn/~maxt/SPelton/reports/report-9aa0d3.html
-//JSON credits: https://github.com/nlohmann/json
 #include <iostream>
-#include "./json.hpp"
+#include <string>
+#include <utility>
 #include <vector>
+
 #include "./http_stuff.h"
+#include "./json.hpp"
+#include "gameState.h"
 using namespace std;
 using namespace nlohmann;
 
-extern string moves;
+extern std::string moves;
 extern int cycle;
 
-int init_data(const json data){
-  cout << "\n\n" << data["board"]["food"]["x"] << "\n\n";
-  vector<int>food;
-  
-  // for (const auto& item : data["board"]["food"].items())
-  //   {
-  //      food.push_back(item);
-  //   }
-  // for (auto &f : food){
-  //   cout << f << endl;
-  // }
-  return 0;
+void init_data(const json data) {
+  // init board
+  Board board;
+  for (int i = 0; i < data["board"]["food"].size(); i++) {
+    board.food.push_back(make_pair(data["board"]["food"][i]["x"],
+                                   data["board"]["food"][i]["y"]));
+  }
+
+  for (int i = 0; i < data["board"]["hazards"].size(); i++) {
+    board.hazards.push_back(make_pair(data["board"]["hazards"][i]["x"],
+                                      data["board"]["hazards"][i]["y"]));
+  }
+
+  board.height = data["board"]["height"];
+  for (int i = 0; i < data["board"]["snakes"].size(); i++) {
+    Snake snake;
+    // head
+    snake.head.first = data["board"]["snakes"][i]["head"]["x"];
+    snake.head.second = data["board"]["snakes"][i]["head"]["y"];
+
+    // health
+    snake.health = data["board"]["snakes"][i]["health"];
+    // id
+    snake.id = data["board"]["snakes"][i]["id"];
+    // latency
+    std::string temp = data["board"]["snakes"][i]["latency"];
+    snake.latency = std::stoi(temp);
+    // length
+    snake.length = data["board"]["snakes"][i]["length"];
+    // name
+    snake.name = data["board"]["snakes"][i]["name"];
+    // shout
+    snake.shout = data["board"]["snakes"][i]["shout"];
+
+    for (int j = 0; j < data["board"]["snakes"][i]["body"].size(); j++) {
+      snake.body.push_back(
+          make_pair(data["board"]["snakes"][i]["body"][j]["x"],
+                    data["board"]["snakes"][i]["body"][j]["y"]));
+    }
+    board.snakes.push_back(snake);
+  }
+
+  board.width = data["board"]["width"];
+  // init game
+  Game game;
+  game.id = data["game"]["id"];
+  game.ruleset.name = data["game"]["ruleset"]["name"];
+  game.ruleset.version = data["game"]["ruleset"]["version"];
+  game.timeout = data["game"]["timeout"];
+  // init Turn
+  Turn turn;
+  turn.turn = data["turn"];
+  // init you
+  You you;
+  Snake snake;
+  snake.head.first = data["you"]["head"]["x"];
+  snake.head.second = data["you"]["head"]["y"];
+  snake.health = data["you"]["health"];
+  snake.id = data["you"]["id"];
+  std::string temp = data["you"]["latency"];
+  snake.latency = std::stoi(temp);
+  snake.name = data["you"]["name"];
+  snake.name = data["you"]["shout"];
+  for (int i = 0; i < data["you"]["body"].size(); i++) {
+    snake.body.push_back(
+        make_pair(data["you"]["body"][i]["x"], data["you"]["body"][i]["y"]));
+  }
+  you.snake = snake;
 }
 
-int move(){
-  // cout << "data: " << data[1] << "\n";
+int move(Board &board, Game &game, Turn &turn, You &you) {
   int index = cycle % 4;
   return index;
 }
